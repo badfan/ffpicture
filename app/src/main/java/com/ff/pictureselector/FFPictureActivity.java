17233a58f8d3310bac2378401c62bd553a357d92
fan <fan>
2020-06-22 18:44:48 +0800

    picture

--- app/src/main/java/com/ff/pictureselector/FFPictureActivity.java
+++ app/src/main/java/com/ff/pictureselector/FFPictureActivity.java
@@ -3,7 +3,7 @@ package com.ff.pictureselector;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
-public class MainActivity extends AppCompatActivity {
+public class FFPictureActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
